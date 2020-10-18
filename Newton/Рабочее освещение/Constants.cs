ab2c419590e298c2f43a049f11b218473c153ad8
Sunshine-ki <sukocheva.alis@mail.ru>
2020-10-18 18:15:14 +0300

    New

---
+++ Newton/Рабочее освещение/Constants.cs
@@ -0,0 +1,33 @@
+namespace Newton
+{
+	public enum SizeObjects
+	{
+		WidthWindows = 1200,
+		HeightWindows = 900,
+		WidthCanvas = 1200,
+		HeightCanvas = 600
+
+	}
+
+	public enum Mode
+	{
+		Off,
+		On
+	}
+
+	public enum State
+	{
+		Start,
+		MovingUpRight,
+		MovingDownRight,
+		MovingUpLeft,
+		MovingDownLeft
+
+	}
+
+	public enum LightType
+	{
+		Point,
+		Ambient
+	}
+}
